235b61df80f157b49a6626b1efe7f23a7c9ef6ee
anton <azvontsov@gmail.com>
2022-11-07 18:35:05 -0500

    adds Layout with children

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Head from "next/head";
+import NavBar from "./NavBar";
+import FooterBanner from "./FooterBanner";
+import Footer from "./Footer";
 
-const Layout = () => {
-  return <div>Layout</div>;
+const Layout = ({ children }) => {
+  return (
+    <div className="layout">
+      <Head>
+        <title>AZ Store</title>
+      </Head>
+      <header>
+        <NavBar />
+      </header>
+      <main className="main-container">{children}</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 };
 
 export default Layout;
