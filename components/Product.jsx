20c87269d242cb199efd500918b0cf9afc52339c
anton <azvontsov@gmail.com>
2022-11-07 18:35:48 -0500

    change order

--- components/Product.jsx
+++ components/Product.jsx
@@ -9,10 +9,10 @@ const Product = ({ product: { image, name, slug, price } }) => {
       <Link href={`/product/${slug.current}`}>
         <div className="product-card">
           <img
+            className="product-image"
             src={urlFor(image && image[0])}
             width={250}
             height={250}
-            className="product-image"
           />
           <p className="product-name">{name}</p>
           <p className="product-price">${price}</p>
