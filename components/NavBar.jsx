7d32d74ebc15f364e16eede0dd9d825dfdc71854
anton <azvontsov@gmail.com>
2022-11-13 12:04:10 -0500

    fix NavBar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <div className="navbar-container">
       <p className="logo">
-        <Link href="/">JSM Headphones</Link>
+        <Link href="/">Misha's Merch</Link>
       </p>
 
       <button type="button" className="cart-icon" onClick="">
